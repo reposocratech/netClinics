19ae767a425a9d7da40c3008f4d4ad08d4c4935c
Jonathan Garrido <blackgokuart777@gmail.com>
2023-01-17 15:07:21 +0100

    changes routes

--- server/app.js
+++ server/app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+
 var usersRouter = require('./routes/users');
+var medicsRouter = require('./routes/medic');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -19,8 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
+app.use('/medics', medicsRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
