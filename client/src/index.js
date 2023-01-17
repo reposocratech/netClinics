8b655fb7e444e0b8edb1aaa335c4880233f833e5
Jonathan Garrido <blackgokuart777@gmail.com>
2023-01-17 10:30:01 +0100

    jonathan

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    console.log("Soy Oscar");
     <App />
   </React.StrictMode>
 );
