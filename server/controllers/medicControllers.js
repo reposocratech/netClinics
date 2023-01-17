a5aae9710e0f53ad1b85bb5e758b2c7252bfd4fa
Jonathan Garrido <blackgokuart777@gmail.com>
2023-01-17 16:30:25 +0100

    cambios jonathan

--- server/controllers/medicControllers.js
+++ server/controllers/medicControllers.js
@@ -3,3 +3,25 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+class medicControllers {
+
+    //1.- createMedic
+    //localhost:4000/medic/createMedic
+
+    createMedic = (req, res) => {
+        
+        const {name, lastname, dni, email, password, medic_membership_number} = req.body
+
+        let saltRounds = 8;
+        bcrypt.genSalt(saltRounds, function (err, saltRounds){
+            bcrypt.hash(password, saltRounds, function (err, hash){
+                
+            })
+        })
+
+    }
+
+
+}
+
+module.exports = new medicControllers();
