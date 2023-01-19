2058074d51900efa5e46a3c850ab2cfbd386ad2c
Jonathan Garrido <blackgokuart777@gmail.com>
2023-01-19 14:25:39 +0100

    changes jonathan

---
+++ client/src/Utils/checkEmail/checkEmail.js
@@ -0,0 +1,15 @@
+//------------------------------------------------------------
+//FUNCION PARA VALIDAR EMAIL
+export const emailValidator = (email) => {
+    
+    let isMail = false;
+    let validator= /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,8})$/;
+    if (validator.test(email)){
+        isMail = true;
+    }
+
+
+    return isMail;
+}
+
+//------------------------------------------------------------
