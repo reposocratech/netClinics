43cb09c7a9c87f5f11897e2c223b1070ace8d0c8
anadlmata <anadelamatagiraldo@gmail.com>
2023-01-19 13:36:24 +0100

    añadir file al registro de Medic

--- client/src/components/Register Medic/RegisterMedic.js
+++ client/src/components/Register Medic/RegisterMedic.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
+import axios from 'axios';
 
 const initialMedicValue = {
     name: '',
     lastname: '',
+    address: '',
+    phone_number: '',
     dni: '',
-    membership_number: '',
     email: '',
+    postal_code: '',
     password: '',
+    province_id: '',
+    city_id: '',
+    medic_membership_number: '',
+    document: '',
 }
 
 export const RegisterMedic = ({showRegisterPatient, setShowRegisterPatient, showRegisterMedic, setShowRegisterMedic}) => {
     const [registerMedic, setRegisterMedic] = useState(initialMedicValue);
+    const [documents, setDocuments] = useState();
+    const [inputDocument, setInputDocument] = useState();
     const [message1, setMessage1] = useState(false);
     const [message2, setMessage2] = useState(false);
+    const [listProvinces, setListProvinces] = useState([]);
+    const [selectedProvince, setSelectedProvince] = useState();
+    const [listCities, setListCities] = useState([]);
+    const [selectedCitie, setSelectedCitie] = useState();
 
     const navigate = useNavigate();
 
@@ -22,19 +35,80 @@ export const RegisterMedic = ({showRegisterPatient, setShowRegisterPatient, show
         setShowRegisterPatient(true);
     }
 
-    const handleChange = () => {
-
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setRegisterMedic({...registerMedic, [name]:value});
     }
 
-    const onSubmit = () => {
-
+    const handleFiles = (e) => {
+        setInputDocument(e.target.files);
     }
+
+    const onSubmit = (event) => {
+        const newFormData = new FormData();
+
+        if(!registerMedic.email || !registerMedic.password){
+            setMessage1(true)
+            setMessage2(false)
+        }
+        else{
+            if(inputDocument){
+                for(const elem of inputDocument){
+                    newFormData.append('file', elem)
+                }
+            }
+            
+            axios
+                .post("http://localhost:4000/medic/createMedic", registerMedic)
+                .then((res) => {
+                    console.log(res);
+                })
+                .catch((error) => {
+                    setMessage1(false)
+                    setMessage2(true)
+                    console.log(error);
+                })
+        }
+    }
+
+    useEffect(() => {
+        axios
+          .get("http://localhost:4000/place/getAllProvince/")
+          .then((res) => {
+            setListProvinces(res.data);
+          })
+          .catch((error) =>{
+            console.log(error);
+          })
+      }, []);
+
+    const getCity = (selectedProvince) => {
+        console.log(selectedProvince);
+        if(selectedProvince){
+           axios
+            .get(`http://localhost:4000/place/getAllCity/${selectedProvince}`)
+            .then((res) => {
+              setListCities(res.data);
+              setRegisterMedic({...registerMedic, province_id : selectedProvince})
+            })
+            .catch((error) => {
+              console.log(error);
+            })
+          }
+      };
+
+    const handleCities = (e) => {
+        // setSelectedProvince(e.target.value);
+        setRegisterMedic({...registerMedic, city_id : e.target.value});
+    }
+
   return (
     <div className='bgColor d-flex flex-column'>
         <button className='borderButton' onClick={showFormPatient}>
         ¿Eres Paciente?. Regístrate aquí.
         </button>
       <div className='whiteBox d-flex flex-column justify-content-center'>
+        <h2> Registro Fisio</h2>
         
           <input
           className='my-3'
@@ -56,6 +130,74 @@ export const RegisterMedic = ({showRegisterPatient, setShowRegisterPatient, show
           onChange={handleChange}
           />
 
+        <input
+          className='mb-3'
+          placeholder='Introduce tu D.N.I'
+          name='dni'
+          type='text'
+          required
+          value={registerMedic.dni}
+          onChange={handleChange}
+          />
+
+          <input
+          className='mb-3'
+          placeholder='Introduce tu Nº de Teléfono'
+          name='phone_number'
+          type='text'
+          required
+          value={registerMedic.phone_number}
+          onChange={handleChange}
+          />
+
+          <input
+          className='mb-3'
+          placeholder='Escribe tu Dirección'
+          name='address'
+          type='text'
+          value={registerMedic.address}
+          onChange={handleChange}
+          />
+
+          <input
+          className='mb-3'
+          placeholder='Introduce tu Código Postal'
+          name='postal_code'
+          type='text'
+          value={registerMedic.postal_code}
+          onChange={handleChange}
+          />
+
+          <input
+          className='mb-3'
+          placeholder='Introduce tu Nº de Colegiado'
+          name='medic_membership_number'
+          type='text'
+          required
+          value={registerMedic.medic_membership_number}
+          onChange={handleChange}
+          />
+
+          <select className='mb-3' name='province'
+          onChange={(e) => getCity(e.target.value)}>
+            <option>Elige Provincia</option>
+            {listProvinces?.map((province) => {
+              return (
+                  <option key={province.province_id} value={province.province_id}>{province.province_name}</option>    
+              )
+            })}
+          </select>
+
+          <select className='mb-3' name='cities'
+          onChange={handleCities}>
+            <option>Elige Ciudad</option>
+            {listCities?.map((city) => {
+              return (
+                  <option key={city.city_id} value={city.city_id}>{city.city_name}</option>    
+              )
+            })}
+          </select>
+
           <input
           className='mb-3'
           placeholder='Escribe tu Email'
@@ -76,6 +218,9 @@ export const RegisterMedic = ({showRegisterPatient, setShowRegisterPatient, show
           value={registerMedic.password}
           onChange={handleChange}
           />
+
+          <label>Adjunta tu Título Profesional y Documento de Número de Colegiado</label>
+          <input type='file' multiple onChange={handleFiles}/>
 
           <div className='mb-2'>
             {message1 && <p>Introduce todos los datos</p>}
