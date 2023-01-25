ca1177a280b44a5acb051a48ab60eb954d017b48
LuisEDominguez <luisdomdi10@hotmail.com>
2023-01-25 13:52:59 +0100

    nuevos cambios en citas paciente

---
+++ client/src/pages/User/UserAppointments/UserAppointmentsHistory.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react'
+import { Table } from 'react-bootstrap'
+import { NetClinicsContext } from '../../../context/NetClinicsProvider';
+
+export const UserAppointmentsHistory = () => {
+    const [appointmentData, setAppointmentData] = useState();
+    const [listMedics, setListMedics] = useState([]);
+    const { user } = useContext(NetClinicsContext);
+
+    useEffect(() => {
+        if(!user.user_id) return
+        axios
+          .get(`http://localhost:4000/patient/getAppointmentHistory/${user.user_id}`)
+          .then((res)=>{
+              // console.log(res.data);
+              setAppointmentData(res.data);
+          })
+          .catch((err) => console.log(err));
+      }, [])
+
+      useEffect(() => {
+        if(!user.user_id) return
+        axios
+          .get(`http://localhost:4000/patient/getMedicsName`)
+          .then((res)=>{
+              // console.log(res.data);
+              setListMedics(res.data);
+          })
+          .catch((err) => console.log(err));
+      }, [])
+      
+      
+      const findMedicName = (id_medic) => {
+        return listMedics?.find((el)=> {
+           if(el.user_id === id_medic){
+               return `${el.name} ${el.lastname}`
+           }
+       });
+     }
+     
+  return (
+    <Table striped>
+      <thead>
+        <tr>
+          <th>Nombre médico</th>
+          <th>Fecha</th>
+          <th>Hora</th>
+          <th>Dirección</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        {appointmentData?.map((appointment,i)=>{
+            return(
+                <tr key={i}>
+                    <td>{findMedicName(appointment.user_medic_id).name} {findMedicName(appointment.user_medic_id).lastname}</td>
+                    <td>{appointment.appointment_date}</td>
+                    <td>{appointment.appointment_time}</td>
+                    <td>{appointment.appointment_address}</td>
+                    
+                </tr>
+            )
+        })}
+        
+       
+      </tbody>
+    </Table>
+  )
+}
--- server/routes/patient.js
+++ server/routes/patient.js
@@ -16,9 +16,9 @@ router.post("/createPatient", multerSingleImage("user"), patientController.creat
 router.get("/onePatient/:user_id", patientController.selectOnePatient);
 
 //------------------------------------------------------
-//3.-Trae la informacion de la busqueda de los medicos
-//localhost:4000/patient/getMedics 
-router.get("/getMedics", patientController.getAllMedics);
+//3.-Trae la informacion de un medico
+//localhost:4000/patient/getMedicsName/:user_id 
+router.get("/getMedicsName", patientController.getMedicsName);
 
 //----------------------------------------------------------------
 //4.-Trae la información de un usuario para modificarla
@@ -29,6 +29,21 @@ router.get("/getEditPatient/:user_id", patientController.getEditPatient);
 //5.-Editar un usuario 
 //localhost:4000/patient/editPatient/:user_id       
 router.put("/editPatient/:user_id", multerSingleImage("user"), patientController.editPatient);
+
+//----------------------------------------------------------------
+//6.-Trae todas las citas realizadas de un paciente
+//localhost:4000/patient/getAppointmentHistory/:user_id  
+router.get("/getAppointmentHistory/:user_id", patientController.getAppointmentHistory);
+
+//----------------------------------------------------------------
+//7.-Trae todas las citas proximas (tanto confirmadas como pendientes de confirmar) de un paciente
+//localhost:4000/patient/getAppointmentHistory/:user_id  
+router.get("/getPendingAppointments/:user_id", patientController.getPendingAppointments);
+
+//----------------------------------------------------------------
+//8.-Trae todas las citas realizadas de un paciente
+//localhost:4000/patient/getAppointmentHistory/:user_id  
+router.get("/getConfirmedAppointments/:user_id", patientController.getConfirmedAppointments);
 
 
 module.exports = router;
