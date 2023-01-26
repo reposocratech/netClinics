5cddd54d4e02369f2e4090c592529eb0135b75ac
Oscar Anaya <oscaranaya1980@gmail.com>
2023-01-26 17:42:37 +0100

    vista perfil médico desde el admin

--- client/src/pages/Admin/MedicProfile/AdminMedicProfile.js
+++ client/src/pages/Admin/MedicProfile/AdminMedicProfile.js
@@ -16,7 +16,8 @@ export const AdminMedicProfile = () => {
   const [dataTitles, setDataTitles] = useState([]);
   const [dataSpecialities, setDataSpecialities] = useState([]);
   const { user_id } = useParams();
-  console.log("este es el user_id", user_id);
+  const [provinceCity, setProvinceCity] = useState();
+  const [providerServices, setProviderServices] = useState([]);
 
   const navigate = useNavigate();
 
@@ -28,6 +29,28 @@ export const AdminMedicProfile = () => {
         setDataUser(res.data.user[0]);
         setDataTitles(res.data.titles);
         setDataSpecialities(res.data.specialities);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/place/getPlaceOneUser/${user_id}`)
+      .then((res) => {
+        setProvinceCity(res.data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/medic/providerServices/${user_id}`)
+      .then((res) => {
+        setProviderServices(res.data);
       })
       .catch((error) => {
         console.log(error);
@@ -58,21 +81,23 @@ export const AdminMedicProfile = () => {
                 src={`/assets/images/user/${dataUser?.avatar}`}
               />
             </div>
-            <h3>
-              {dataUser?.name} {dataUser?.lastname}
-            </h3>
+            <h2 className="my-4">
+              <strong className="name">{dataUser?.name}</strong>{" "}
+              <strong className="lastname">{dataUser?.lastname}</strong>
+            </h2>
           </Col>
-          <Col sm="12" md="4" className="d-flex justify-content-end"></Col>
+          <Col sm="12" md="4" className="d-flex justify-content-end">
+          </Col>
         </Row>
         <Row className="ms-2 me-2 mb-3">
-          <Col sm="12" md="12" className="fondos_Sections">
+          <Col sm="12" md="12" className="section">
             <h4>Sobre mí</h4>
             <hr className="separador" />
             <p>{dataUser?.medic_description}</p>
           </Col>
         </Row>
-        <Row className="ms-2 me-2 gap-4">
-          <Col className="fondos_Sections">
+        <Row className="ms-2 me-2 d-flex flex-row justify-content-between gap-3">
+          <Col sm="12" md="12" lg="3" className="section">
             <h4>Datos Personales</h4>
             <hr className="separador mb-3" />
             <label className="campos">Nombre:</label>
@@ -83,10 +108,50 @@ export const AdminMedicProfile = () => {
             <p>{dataUser?.phone_number}</p>
             <label className="campos">Email:</label>
             <p>{dataUser?.email}</p>
+            <label className="campos">Ciudad:</label>
+            <p>{provinceCity?.city_name}</p>
+            <label className="campos">Provincia:</label>
+            <p>{provinceCity?.province_name}</p>
+            <label className="campos">Código Postal:</label>
+            <p>{dataUser?.postal_code}</p>
+          </Col>
+          <Col sm="12" md="12" lg="3" className="section">
+            <h4>Listado Especialidades</h4>
+            <hr className="separador" />
+            <label className="campos">Especialidades:</label>
+            <ul className="my-3">
+              {dataSpecialities?.map((speciality) => {
+                return (
+                  <li key={speciality?.speciality_name}>
+                    {speciality?.speciality_name}
+                  </li>
+                );
+              })}
+            </ul>
+          </Col>
+          <Col sm="12" md="12" lg="3" className="section">
+            <h4>Prestación Servicios</h4>
+            <hr className="separador mb-3" />
+            <label className="campos">Provincia & Ciudad Servicios:</label>
+            <ul className="my-3">
+              {providerServices?.map((services, i) => {
+                return (
+                  <li
+                    key={i}
+                  >{`${services.province_name} (${services.city_name})`}</li>
+                );
+              })}
+            </ul>
+          </Col>
+          <Col sm="12" md="12" lg="2" className="section">
+            <h4>Tarifa Servicio</h4>
+            <hr className="separador mb-3" />
+            <label className="campos">Precio:</label>
+            <p>{dataUser?.medic_price} €</p>
           </Col>
         </Row>
         <Row className="ms-2 me-2 my-3 mb-3">
-          <Col sm="12" md="12" className="fondos_Sections">
+          <Col sm="12" md="12" className="section">
             <h4>Datos Académicos</h4>
             <hr className="separador" />
             <Table className="my-2 text-center my-3" striped bordered hover>
@@ -96,7 +161,7 @@ export const AdminMedicProfile = () => {
                   <th>Universidad</th>
                   <th>Fecha Inicio</th>
                   <th>Fecha Fin</th>
-                  <th>Descargar Titulo</th>
+                  <th>Descargar</th>
                 </tr>
               </thead>
               <tbody style={{ cursor: "pointer" }}>
@@ -127,17 +192,6 @@ export const AdminMedicProfile = () => {
                 })}
               </tbody>
             </Table>
-          </Col>
-        </Row>
-        <Row className="ms-2 me-2 my-3 mb-3">
-          <Col sm="12" md="12" className="fondos_Sections">
-            <h4>Especialidades</h4>
-            <hr className="separador" />
-            <ul className="my-3">
-              {dataSpecialities?.map((speciality) => {
-                return <li>{speciality?.speciality_name}</li>;
-              })}
-            </ul>
           </Col>
         </Row>
         <div className="d-flex justify-content-center">
